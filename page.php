61c2a6037fea51729d27b7879ac17a01eedee9b6
Chazona Baum <chznbaum@gmail.com>
2017-12-11 18:14:53 -0500

    added comments to pages

--- page.php
+++ page.php
@@ -1,20 +1,22 @@
 <?php get_header(); ?>
 
-  <main>
+<main>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <h1><?php the_title(); ?></h1>
+    <h1><?php the_title(); ?></h1>
 
-        <?php the_content(); ?>
+    <?php the_content(); ?>
+
+    <?php comments_template(); ?>
       
-    <?php endwhile; else : ?>
+  <?php endwhile; else : ?>
 
-      <h1><?php _e( 'Nothing to See Here', 'riada' ); ?></h1>
-      <p><?php _e( 'There doesn\'t seem to be any content here.', 'riada' ); ?></p>
+    <h1><?php _e( 'Nothing to See Here', 'riada' ); ?></h1>
+    <p><?php _e( 'There doesn\'t seem to be any content here.', 'riada' ); ?></p>
 
-    <?php endif; ?>
+  <?php endif; ?>
 
-  </main>
+</main>
 
 <?php get_footer(); ?>
