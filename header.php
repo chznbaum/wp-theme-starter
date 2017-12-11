67108317c6008aabd89dd8317adcf5a41b486923
Chazona Baum <chznbaum@gmail.com>
2017-12-11 11:47:03 -0500

    connected header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Riada</title>
+    <?php wp_head(); ?>
+  
+    <!-- build:css ./styles.css -->
+    <link rel="stylesheet" href="./temp/css/styles.css">
+    <!-- endbuild -->
+
+    <!-- build:js ./assets/js/vendor.js -->
+    <script src="/temp/js/vendor.js"></script>
+    <!-- endbuild -->
+
+  </head>
+  <body <?php body_class(); ?>>
+    <header>
+      <nav>
+      </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Riada</title>
-  
-    <!-- build:css ./styles.css -->
-    <link rel="stylesheet" href="./temp/css/styles.css">
-    <!-- endbuild -->
-
-    <!-- build:js ./assets/js/vendor.js -->
-    <script src="/temp/js/vendor.js"></script>
-    <!-- endbuild -->
-
-  </head>
-  <body>
-
+<?php get_header(); ?>
     <main>
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
