b94486ee8dc324c8f500c082a8511a9f6e3b9795
Chazona Baum <chznbaum@gmail.com>
2017-12-11 18:12:20 -0500

    added pagination to index

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 
   <main>
 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ) :
+      while ( have_posts() ) : the_post(); ?>
 
       <h2>
         <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
       </h2>
 
-      <?php the_content(); ?>
+      <?php the_content();
       
-    <?php endwhile; else : ?>
+      endwhile;
+
+      the_posts_pagination();
+
+    else : ?>
 
       <h2><?php _e( 'Nothing to See Here', 'riada' ); ?></h2>
       <p><?php _e( 'There doesn\'t seem to be any posts here.', 'riada' ); ?></p>
