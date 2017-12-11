a87803895f5b01cd48f3d070ecf0be4c9751b399
Chazona Baum <chznbaum@gmail.com>
2017-12-11 15:33:43 -0500

    added page.php

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+  <main>
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h1><?php the_title(); ?></h1>
+
+        <?php the_content(); ?>
+      
+    <?php endwhile; else : ?>
+
+      <h1><?php _e( 'Nothing to See Here', 'riada' ); ?></h1>
+      <p><?php _e( 'There doesn\'t seem to be any content here.', 'riada' ); ?></p>
+
+    <?php endif; ?>
+
+  </main>
+
+<?php get_footer(); ?>
