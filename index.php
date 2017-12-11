f21cf6730da00a9a81107c869c6da98f9e03277e
Chazona Baum <chznbaum@gmail.com>
2017-12-11 11:31:04 -0500

    started index.php

--- index.php
+++ index.php
@@ -15,6 +15,26 @@
 
   </head>
   <body>
+
+    <main>
+
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <h2>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </h2>
+
+        <?php the_content(); ?>
+        
+      <?php endwhile; else : ?>
+
+        <h2><?php _e( 'Nothing to See Here', 'riada' ); ?></h2>
+        <p><?php _e( 'There doesn\'t seem to be any posts here.', 'riada' ); ?></p>
+
+      <?php endif; ?>
+
+    </main>
+
     <!-- build:js ./assets/js/app.js -->
     <script src="/temp/js/app.js"></script>
     <!-- endbuild -->
