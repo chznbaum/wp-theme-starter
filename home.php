5e3deeb1b1f2733df1fa184d73b9f10a80cebf24
Chazona Baum <chznbaum@gmail.com>
2017-12-11 21:58:02 -0500

    added home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * The template for displaying blog posts */
+get_header(); ?>
+
+<main>
+
+  <?php if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+
+    <h2>
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    </h2>
+
+    <?php the_content();
+      
+    endwhile;
+
+    the_posts_pagination();
+
+  else : ?>
+
+    <h2><?php _e( 'Nothing to See Here', 'riada' ); ?></h2>
+    <p><?php _e( 'There doesn\'t seem to be any posts here.', 'riada' ); ?></p>
+
+  <?php endif; ?>
+
+</main>
+
+<?php get_footer(); ?>
